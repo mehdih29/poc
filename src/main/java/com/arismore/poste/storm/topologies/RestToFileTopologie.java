456abf976f8eb752874ca7e6d8af3b4f7590fc4a
mehdih29 <mehdih29@gmail.com>
2014-11-13 12:32:12 +0100

    change the conf

--- src/main/java/com/arismore/poste/storm/topologies/RestToFileTopologie.java
+++ src/main/java/com/arismore/poste/storm/topologies/RestToFileTopologie.java
@@ -1,10 +1,8 @@
 package com.arismore.poste.storm.topologies;
 
 import backtype.storm.Config;
-import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
 import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.utils.Utils;
 
 import com.arismore.poste.storm.bolts.JobStarterBolt;
 import com.arismore.poste.storm.bolts.UriGetBolt;
@@ -24,22 +22,33 @@ public class RestToFileTopologie {
 		builder.setBolt("jobStarter", new JobStarterBolt(), 1)// .setNumTasks(2)
 				.shuffleGrouping("slidingWindow");
 		// --> 200/100= 2 tasks per executors
-		builder.setBolt("uriGet", new UriGetBolt(), 6).setNumTasks(1)
+		builder.setBolt("uriGet", new UriGetBolt(), 6).setNumTasks(10)
 				.shuffleGrouping("jobStarter");
-		
+
 		builder.setBolt("tofile", new WriteToFileBolt(), 4).setNumTasks(8)
-		.shuffleGrouping("uriGet");
+				.shuffleGrouping("uriGet");
 
 		Config conf = new Config();
 		// conf.setMessageTimeoutSecs(120);
 		// Config.STORM_ZOOKEEPER_SESSION_TIMEOUT =
-		
 
-		/* The maximum amount of time given 
-		** to the topology to fully process
-		** a message emitted by a spout */
+		/*
+		 * The maximum amount of time given* to the topology to fully process* a
+		 * message emitted by a spout
+		 */
 		conf.put(Config.TOPOLOGY_MESSAGE_TIMEOUT_SECS, 600);
+		
+		/* How long without heartbeating a task can go 
+		** before nimbus will consider the task dead 
+		** and reassign it to another location
+		* */
 		conf.put(Config.NIMBUS_TASK_TIMEOUT_SECS, 500);
+
+		/* How long before a supervisor can go without 
+		** heartbeating before nimbus considers it dead 
+		** and stops assigning new work to it
+		**/
+		conf.put(Config.NIMBUS_SUPERVISOR_TIMEOUT_SECS, 600);
 
 		conf.setDebug(false);
 
