5594f40dc08b63e071be722db9a6c832c06d7f22
mehdih29 <mehdih29@gmail.com>
2015-01-08 15:57:02 +0100

    manage the packages

--- src/main/java/com/arismore/poste/util/Singleton.java
+++ src/main/java/com/arismore/poste/util/Singleton.java
@@ -1,4 +1,8 @@
-package com.arismore.poste.data;
+package com.arismore.poste.util;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -6,22 +10,22 @@ import com.google.gson.GsonBuilder;
 public class Singleton {
 
 	Gson gson;
-	//Client client;
+	Client client;
 
 	private Singleton() {
 		this.gson = new GsonBuilder().create();
-		/*this.client = new TransportClient()
+		this.client = new TransportClient()
 				.addTransportAddress(new InetSocketTransportAddress(
-						"127.0.0.1", 9300));*/
+						"127.0.0.1", 9300));
 	}
 
 	public Gson getGson() {
 		return gson;
 	}
 
-	/*public Client getClient() {
+	public Client getClient() {
 		return client;
-	}*/
+	}
 
 	/** Holder */
 	private static class SingletonHolder {
